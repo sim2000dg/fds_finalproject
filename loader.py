ba015f3cf828eea2036a7669c74bdf87a1ef8596
sim2000dg <sim2000dg@gmail.com>
2022-12-13 10:02:22 +0100

    test (Giacomo)

--- loader.py
+++ loader.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import LabelEncoder
 import pickle
 import numpy as np
 import warnings
-import math
+import time
 
 # Fit encoders in order to map string labels to integers
 # The fact that we are storing the scikit objects makes the encoding reproducible and consistent
@@ -32,7 +32,8 @@ class CardsDataset(Dataset):
     labels, which are also reliably encoded.
     """
 
-    def __init__(self, csv_path: str, root_dir: str, transform: str, card_category: bool, label_encoder: LabelEncoder,
+    def __init__(self, csv_path: str, root_dir: str, transform: str,
+                 card_category: bool, label_encoder: LabelEncoder,
                  subset: str = 'train') -> None:
         """
         Initialization of the CardDataset class
@@ -66,31 +67,36 @@ class CardsDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_path = os.path.join(self.root_dir, self.cards_table.iat[idx, 1])
-        image = io.imread(img_path)
-        card_class = self.cards_table.iat[idx, 2] if self.card_category else self.cards_table.iat[idx, 3]
+        card_class = self.cards_table.iat[idx, 2] if not self.card_category else self.cards_table.iat[idx, 3]
 
         if self.transform == 'hog':
-            representation = np.squeeze(hog(image, channel_axis=2, block_norm='L2', pixels_per_cell=(4, 4),
-                                            cells_per_block=(1, 1), feature_vector=False))
-            block_view = view_as_blocks(representation, (4, 4, 9))  # This is a view on the original array
-            block_view /= np.linalg.norm(
-                np.reshape(np.squeeze(block_view), (block_view.shape[0], block_view.shape[1], -1)), axis=2,
-                ord=2)[:, :, np.newaxis, np.newaxis, np.newaxis, np.newaxis]
+            representation = np.load(os.path.join(self.root_dir, 'hog',
+                                                  self.cards_table.iat[idx, 1].split('.')[0] + '.npy'))
+            # block_view = view_as_blocks(representation, (4, 4, 9))  # This is a view on the original array
+            # block_view /= np.linalg.norm(
+            #     np.reshape(np.squeeze(block_view), (block_view.shape[0], block_view.shape[1], -1)), axis=2,
+            #     ord=2)[:, :, np.newaxis, np.newaxis, np.newaxis, np.newaxis] + 0.01
             representation = np.moveaxis(representation, 2, 0)  # channel first view, as required for PyTorch
-        elif self.transform == 'rgb_hist':
-            representation, _ = np.histogramdd((np.reshape(image, (-1, 3))), bins=[np.linspace(0, 256, 6)] * 3)
-            representation = (representation / representation.sum()).flatten()
+        elif self.transform is None or self.transform == 'rgb_hist':
+            img_path = os.path.join(self.root_dir, self.cards_table.iat[idx, 1])
+            image = io.imread(img_path)
+            if self.transform == 'rgb_hist':
+                representation, _ = np.histogramdd((np.reshape(image, (-1, 3))), bins=[np.linspace(0, 256, 6)] * 3)
+                representation = (representation / representation.sum()).flatten()
+            else:
+                representation = image/255.0
         else:
             raise ValueError(f'There is no transformation function for \'{self.transform}\'')
 
-        return representation, card_class
+        return representation.astype(np.float32), card_class
 
 
 if __name__ == '__main__':
     training_hog = CardsDataset(csv_path=os.path.join('dataset_cards', 'cards.csv'), root_dir='dataset_cards',
-                                transform='hog', card_category=False,
+                                transform=None, card_category=False,
                                 label_encoder=label_encoder_specific, subset='train')
     train_dataloader = DataLoader(training_hog, batch_size=16, shuffle=True)
-    image_batch = next(iter(train_dataloader))
-    print(image_batch[0])
+    start = time.time()
+    for x in train_dataloader:
+        pass
+    print(time.time() - start)
